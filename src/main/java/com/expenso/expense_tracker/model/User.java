d47273906bf9ef2c79028d2ab2309f9d148ffafa
Mr. Aditya <Connect2RajAditya@gmail.com>
2025-12-19 10:31:51 +0530

    Upgrade Java runtime to Java 21 LTS

--- src/main/java/com/expenso/expense_tracker/model/User.java
+++ src/main/java/com/expenso/expense_tracker/model/User.java
@@ -1,7 +1,10 @@
 package com.expenso.expense_tracker.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.UUID;
 import java.time.LocalDateTime;
 
